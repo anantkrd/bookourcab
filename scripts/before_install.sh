4fe851f58c5f809b52e8a06651445ab0bcaf2a12
anantkrd21 <89842393+anantkrd21@users.noreply.github.com>
2022-07-02 21:27:34 +0530

    update

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ec2-user/bookourcab
-url -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
+curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
 yum install nodejs npm
