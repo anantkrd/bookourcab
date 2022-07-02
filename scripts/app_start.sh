7032fc0cdd820b3f74ca45792a37425e638bbe60
anantkrd21 <89842393+anantkrd21@users.noreply.github.com>
2022-07-02 13:18:47 +0530

    add deplyemnt

---
+++ scripts/app_start.sh
@@ -0,0 +1,5 @@
+cd /home/ec2-user/bookourcab
+pm2 start node_modules/react-scripts/scripts/start.js --name "bookourcab"
+pm2 startup
+pm2 save
+pm2 restart bookourcab
---
+++ scripts/before_install.sh
@@ -0,0 +1,2 @@
+cd /home/ec2-user/bookourcabapis
+yum -y install nodejs npm
