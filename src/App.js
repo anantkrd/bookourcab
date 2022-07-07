bd527414f56971a51df61670ea80bfda4a417940
anantkrd21 <89842393+anantkrd21@users.noreply.github.com>
2022-07-07 23:10:08 +0530

    build

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router,Route,  Switch } from "react-router-dom";
+import { BrowserRouter,Route,  Switch } from "react-router-dom";
 import Header from './component/Header';
 import Home from './component/Home';
 //import AboutUs from './component/Home';
@@ -43,49 +43,49 @@ import TripComplete from './component/Driver/TripComplete';
 function App() {
   return (
     
-    <Router>
-    <Switch>  
-      <Route path='/Home/' component={Home}></Route>  
-      <Route path='/admin/Home/' component={AdminHome}></Route>        
-      <Route path='/admin/newBookings/' component={AdminHome}></Route>         
-      <Route path='/Plans' component={Plans}></Route> 
-      <Route path='/Servises' component={Servises}></Route>  
-      <Route path='/Login' component={Login}></Route>  
-      <Route path='/History' component={History}></Route>         
-      <Route path='/termCondition' component={TermCondition}></Route>   
-      <Route path='/privacyPolicy' component={PrivacyPolicy}></Route>   
-      
+    <BrowserRouter basename='/'>
+      <Switch>  
+        <Route path='/Home/' component={Home}></Route>  
+        <Route path='/admin/Home/' component={AdminHome}></Route>        
+        <Route path='/admin/newBookings/' component={AdminHome}></Route>         
+        <Route path='/Plans' component={Plans}></Route> 
+        <Route path='/Servises' component={Servises}></Route>  
+        <Route path='/Login' component={Login}></Route>  
+        <Route path='/History' component={History}></Route>         
+        <Route path='/termCondition' component={TermCondition}></Route>   
+        <Route path='/privacyPolicy' component={PrivacyPolicy}></Route>   
+        
 
-      <Route path='/admin/Booking' component={Booking}></Route>  
-      <Route path='/admin/SearchLog' component={SearchLog}></Route>  
-      <Route path='/admin/completed' component={Completed}></Route>  
-      <Route path='/admin/ready' component={ReadyToGo}></Route>  
-      <Route path='/admin/driverWaiting' component={WaitingForDriver}></Route>  
-      <Route path='/admin/agentWaiting' component={WaitingForAgent}></Route>  
-      <Route path='/BookingDetails/:bookingId' component={BookingDetails}></Route>  
-      <Route path='/ConfirmBooking/:data' component={ConfirmBooking}></Route>  
-      <Route path='/ThankYou/:orderId' component={ThankYou}></Route>  
-      <Route path='/Search2/:pickup/:destination/:pickdate/:returnDate/:pickupLocation/:destinationLocation/:mobileNo' component={Search}></Route>
-      <Route path='/Search/:data' component={Search}></Route>
-       
-      <Route path='/agent/Login' component={Partner}></Route>   
-      <Route path='/agent/Home' component={BookingList}></Route>   
-      <Route path='/agent/myPendingBookings' component={PendingBookingList}></Route>   
-      <Route path='/agent/myCompletedBookings' component={CompletedBookingList}></Route>   
-      <Route path='/agent/Profile' component={Profile}></Route>   
-      <Route path='/agent/AddCar' component={AddCar}></Route>   
-      <Route path='/agent/AddDriver' component={AddDriver}></Route>   
-      <Route path='/driver/home' component={Driver}></Route>   
-      <Route path='/driver/booking-report' component={BookingReport}></Route>   
-      <Route path='/driver/payment-report' component={PaymentReport}></Route>   
-      <Route path='/driver/profile' component={DriverProfile}></Route>    
-      <Route path='/driver/trip-details/:bookingId' component={TripDetails}></Route>   
-      <Route path='/driver/trip-complete/:bookingId' component={TripComplete}></Route>  
-      
-      
-      <Header/>
-    </Switch>
-    </Router>
+        <Route path='/admin/Booking' component={Booking}></Route>  
+        <Route path='/admin/SearchLog' component={SearchLog}></Route>  
+        <Route path='/admin/completed' component={Completed}></Route>  
+        <Route path='/admin/ready' component={ReadyToGo}></Route>  
+        <Route path='/admin/driverWaiting' component={WaitingForDriver}></Route>  
+        <Route path='/admin/agentWaiting' component={WaitingForAgent}></Route>  
+        <Route path='/BookingDetails/:bookingId' component={BookingDetails}></Route>  
+        <Route path='/ConfirmBooking/:data' component={ConfirmBooking}></Route>  
+        <Route path='/ThankYou/:orderId' component={ThankYou}></Route>  
+        <Route path='/Search2/:pickup/:destination/:pickdate/:returnDate/:pickupLocation/:destinationLocation/:mobileNo' component={Search}></Route>
+        <Route path='/Search/:data' component={Search}></Route>
+        
+        <Route path='/agent/Login' component={Partner}></Route>   
+        <Route path='/agent/Home' component={BookingList}></Route>   
+        <Route path='/agent/myPendingBookings' component={PendingBookingList}></Route>   
+        <Route path='/agent/myCompletedBookings' component={CompletedBookingList}></Route>   
+        <Route path='/agent/Profile' component={Profile}></Route>   
+        <Route path='/agent/AddCar' component={AddCar}></Route>   
+        <Route path='/agent/AddDriver' component={AddDriver}></Route>   
+        <Route path='/driver/home' component={Driver}></Route>   
+        <Route path='/driver/booking-report' component={BookingReport}></Route>   
+        <Route path='/driver/payment-report' component={PaymentReport}></Route>   
+        <Route path='/driver/profile' component={DriverProfile}></Route>    
+        <Route path='/driver/trip-details/:bookingId' component={TripDetails}></Route>   
+        <Route path='/driver/trip-complete/:bookingId' component={TripComplete}></Route>  
+        
+        
+        <Header/>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
