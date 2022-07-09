17cdce720c0d4dc1796180e04561a53e6f65e944
anantkrd21 <89842393+anantkrd21@users.noreply.github.com>
2022-07-09 12:23:45 +0530

    build

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { Route, Switch, HashRouter as Router } from "react-router-dom";
 import Header from './component/Header';
 import Home from './component/Home';
 //import AboutUs from './component/Home';
